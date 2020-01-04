29a6f0720ef1f9ee7cbaf2b5dd9eebd8d2e62f41
Jon Roobottom <roo@roodesign.co.uk>
2020-01-04 11:39:54 +0000

    renamed `less` to `_less`

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const { src, dest, watch, series } = require('gulp')
 const less = require('gulp-less')
 
 const compile_css = () => {
-  return src('./_source/less/roodesign.less')
+  return src('./_source/_less/roodesign.less')
   .pipe(less())
   .pipe(dest('./_site/assets'))
 }
 exports.default = (callback) => {
-  watch('./_source/less/**/*.less', { ignoreInitial: false }, compile_css)
+  watch('./_source/_less/**/*.less', { ignoreInitial: false }, compile_css)
   callback()
 }
 exports.build = series(compile_css)
